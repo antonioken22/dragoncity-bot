63da09b9928ea85479b466e7330bdc80c4633237
Kenette John A. Antonio <antonio.kenjohn15@gmail.com>
2024-02-28 16:51:49 +0800

    clean

--- watch_greenhouse_ad_bot/watch_greenhouse_ad_bot.py
+++ watch_greenhouse_ad_bot/watch_greenhouse_ad_bot.py
@@ -1,10 +1,11 @@
-import tkinter as tk
-from tkinter import messagebox
 import pyautogui
 import time
 import ctypes
 import sys
 import os
+import msvcrt
+import tkinter as tk
+from tkinter import messagebox
 
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 
