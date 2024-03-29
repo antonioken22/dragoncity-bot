c128fd462146e3451fe07764a259170ec05c7554
Kenette John A. Antonio <antonio.kenjohn15@gmail.com>
2024-03-29 21:45:01 +0800

    Hatchery, Breeding, and Buildings to be skipped must be clicked first to work.

--- skip_6h_bot/skip_6h_bot.py
+++ skip_6h_bot/skip_6h_bot.py
@@ -14,20 +14,16 @@ from options import show_options_dialog
 folder_location = 'skip_6h_bot/'
 window_name = "Skip 6h Bot"
 game_location = "Hatchery, Breeding, and Buildings"
-image_hint = "Skip 6h Play Button"
+image_hint = "Skip 6h Text Button"
 
 # For terminal printing only
 GREEN = '\033[92m'
 RED = '\033[91m'
 RESET = '\033[0m'
 
-# Set uniform values here
-uniform_delay = 0.5
-uniform_confidence = 0.8
-
 def locate_and_click(image_path, success_message, click=True, delay_execution=0):
     try:
-        position = pyautogui.locateOnScreen(image_path, confidence=uniform_confidence)
+        position = pyautogui.locateOnScreen(image_path, confidence=0.8)
         if position is not None:
             print(GREEN + f"Found {success_message} at: {position}" + RESET)
             if click:
@@ -46,18 +42,18 @@ def show_dialog(message):
 # Bot sequence of tasks
 def bot_cycle():
     while True:
-        if locate_and_click(folder_location + 'out-of-ads.png', "Out of Ads found", click=False):
+        if locate_and_click(folder_location + 'out-of-ads.png', "Out of Ads", click=False):
                 show_dialog("No Ads this time, try again later.")
                 show_options_dialog()
                 break
-        elif locate_and_click(folder_location + 'close-reward.png', 'Close Reward Button found', delay_execution=5):
+        elif locate_and_click(folder_location + 'close-reward.png', 'Close Reward Button', delay_execution=5):
                 continue
-        elif locate_and_click(folder_location + 'place.png', 'Place Button found', click=False):
-                locate_and_click(folder_location + 'close.png', 'Close Button found')
+        elif locate_and_click(folder_location + 'place.png', 'Place Button', click=False):
+                locate_and_click(folder_location + 'close.png', 'Close Button')
                 continue
         tasks = [
-            (folder_location + 'skip-6h-play-button.png', 'Skip 6h Play Button found'),
-            (folder_location + 'skip-6h.png', 'Skip 6h Text Button found'),
+            (folder_location + 'skip-6h-play-button.png', 'Skip 6h Play Button'),
+            (folder_location + 'skip-6h.png', 'Skip 6h Text Button'),
         ]
         for task in tasks:
             locate_and_click(*task)
@@ -67,7 +63,9 @@ root = tk.Tk()
 root.withdraw()
 
 confirmation = messagebox.askquestion(window_name, "Locate " + game_location + " and make sure it's in MAXIMIZE window mode. "
-                                                        "Ensure it's the FRONTEST program currently running and not blocked by any other program. \n\nHave you followed everything?")
+                                                        "Click which of it you want its time to be reduced by 6 hours. "
+                                                        "Ensure it's the FRONTEST program currently running and not blocked by any other program. "
+                                                        "\n\nHave you followed everything?")
 
 root.destroy()
 
@@ -78,19 +76,20 @@ if confirmation == 'no':
 # Step 2: Attempt to locate
 while confirmation == 'yes':
     try:
-        position = pyautogui.locateOnScreen(folder_location + 'skip-6h-play-button.png', confidence=0.8)
-        if position is not None:
-            print(GREEN + f"{image_hint} is found at: {position}" + RESET)
-            pyautogui.moveTo(position, duration=0.66)
-            pyautogui.click(position, duration=0.01)
+        skip_6h_text = pyautogui.locateOnScreen(folder_location + 'skip-6h.png', confidence=0.8)
+        if skip_6h_text is not None:
+            print(GREEN + f"{image_hint} is found at: {skip_6h_text}" + RESET) or print
+            pyautogui.moveTo(skip_6h_text, duration=0.66)
+            pyautogui.click(skip_6h_text, duration=0.01)
             bot_cycle()
             break
     except pyautogui.ImageNotFoundException:
         print(RED + f"Could not locate {image_hint}." + RESET)
         answer = messagebox.askyesno(window_name, "Are you sure " + game_location + " is located?\n\n"
                                                         "Locate " + game_location + " and make sure it's in MAXIMIZE window mode. "
-                                                        "Ensure it's the FRONTEST program currently running and not blocked by any other program.\n\n"
-                                                        "Would you like to try scanning again?")
+                                                        "Click which of it you want its time to be reduced by 6 hours. "
+                                                        "Ensure it's the FRONTEST program currently running and not blocked by any other program. "
+                                                        "\n\nWould you like to try scanning again?")
         if not answer:
             show_options_dialog()
             break
